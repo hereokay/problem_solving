7302835300e8822bef424ac8ab2b5d73f1b77f95
hereokay <hereokay88@gmail.com>
2023-09-08 17:00:56 +0900

    2

---
+++ week_4_0904_0910/BOJ_10799_쇠막대기.java
@@ -0,0 +1,34 @@
+package week_4_0904_0910;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_10799_쇠막대기 {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int length = s.length();
+        Stack stack = new Stack<Integer>();
+        int ans = 0;
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c=='('){
+                stack.add(1);
+            }
+            else if (c==')') {
+                stack.pop();
+                if (s.charAt(i-1)=='('){
+                    ans += stack.size();
+                }
+                else{
+                    ans+=1;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
