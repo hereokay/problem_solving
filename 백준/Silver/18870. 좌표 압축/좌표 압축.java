07cdfd6e890fd9f0d407420d9f19ec6c26c19962
hereokay <hereokay88@gmail.com>
2023-10-27 16:59:21 +0900

    [Silver II] Title: 좌표 압축, Time: 1980 ms, Memory: 280848 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -3,17 +3,6 @@ import java.util.*;
 
 class Main {
 
-    static class Node{
-        int origin;
-        int rank;
-        int value;
-
-        public Node(int origin, int value) {
-            this.origin = origin;
-            this.value = value;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,45 +10,35 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
         String[] s = br.readLine().split(" ");
-        Node[] array = new Node[n];
-        for (int i = 0; i < s.length; i++) {
-            int a = Integer.parseInt(s[i]);
-            array[i] = new Node(i,a);
-        }
-
-        Arrays.sort(array,rankComp);
-        int ranking=0;
+        int[] array = new int[n];
         for (int i = 0; i < n; i++) {
-            array[i].rank=ranking;
+            int i1 = Integer.parseInt(s[i]);
+            array[i]=i1;
+        }
+        int[] clone = array.clone();
+        Arrays.sort(array);
+        int ranking=0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            map.put(array[i],ranking);
             if (i != n-1){
-                if (array[i].value != array[i+1].value){
-                    ranking++;
+                if (array[i] != array[i+1]){
+                    ranking+=1;
                 }
             }
         }
 
-        Arrays.sort(array,originComp);
         for (int i = 0; i < n; i++) {
-            bw.write(String.valueOf(array[i].rank));
-            bw.write(" ");
+            int value = clone[i];
+            Integer rank = map.get(value);
+            bw.write(String.valueOf(rank)+" ");
         }
+
+
 
         bw.flush();
         bw.close();
         br.close();
 
     }
-
-    public static Comparator<Node> rankComp = new Comparator<Node>() {
-        @Override
-        public int compare(Node o1, Node o2) {
-            return o1.value - o2.value;
-        }
-    };
-    public static Comparator<Node> originComp = new Comparator<Node>() {
-        @Override
-        public int compare(Node o1, Node o2) {
-            return o1.origin - o2.origin;
-        }
-    };
 }
