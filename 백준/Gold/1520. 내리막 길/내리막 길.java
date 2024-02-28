1aeba99a3b077c5bbd32c24dc8a8db9794a5652c
hereokay <hereokay88@gmail.com>
2024-02-28 17:46:01 +0900

    [Gold III] Title: 내리막 길, Time: 404 ms, Memory: 41688 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -12,69 +12,67 @@ public class Main {
     static class Node {
         int x;
         int y;
-        int hei ;
-        public Node (int x,int y,int hei){
+        public Node (int x,int y){
             this.x = x;
             this.y = y;
-            this.hei = hei;
         }
     }
 
+    static int x;
+    static int y;
     public static void main(String[] args) throws IOException {
 
         String[] inp = br.readLine().split(" ");
-        int x = Integer.parseInt(inp[0]); // 세로
-        int y = Integer.parseInt(inp[1]); // 가로
+        x = Integer.parseInt(inp[0]); // 세로
+        y = Integer.parseInt(inp[1]); // 가로
 
         int[][] board = new int[x+1][y+1];
         boolean[][] visited = new boolean[x+1][y+1];
-        int[][] cnt = new int[x+1][y+1];
+        int[][] dp = new int[x+1][y+1];
+
 
         for(int i=0;i<x;i++){
             inp = br.readLine().split(" ");
             for(int j=0;j<y;j++){
                 board[i][j] = Integer.parseInt(inp[j]);
+                dp[i][j] = -1;
             }
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>(){
-            @Override
-            public int compare(Node a,Node b){
-                return -(a.hei - b.hei);
-            }
-        });
+        int ans = dfs(new Node(0,0),board,dp);
+        System.out.println(ans);
+    }
 
-        pq.add(new Node(0,0,board[0][0]));
+    public static int dfs(Node cur, int[][] board, int[][] dp){
+
+        if(cur.x == x-1 && cur.y == y-1){
+            return 1;
+        }
 
         int[] dy = {1,-1,0,0};
         int[] dx = {0,0,1,-1};
 
-        cnt[0][0] = 1;
-        while(!pq.isEmpty()){
+        int cnt = 0;
+        for(int i=0;i<4;i++){
 
-            Node cur = pq.poll();
+            int nx = dx[i]+cur.x;
+            int ny = dy[i] + cur.y;
 
-            for(int i=0;i<4;i++){
-                int nx = dx[i] + cur.x;
-                int ny = dy[i] + cur.y;
+            if(nx < 0 || nx >=x || ny <0 || ny>= y){
+                continue;
+            }
 
-                if(nx <0 || nx >= x || ny <0 || ny >= y){
-                    continue;
+
+            if(board[nx][ny] < board[cur.x][cur.y]){
+                // 시작
+                if(dp[nx][ny] == -1){
+                    // 기존 데이터가 없으면 DFS 진행
+                    dp[nx][ny] = dfs(new Node(nx, ny), board, dp);
                 }
-
-                if(board[cur.x][cur.y] <= board[nx][ny]){
-                    continue;
-                }
-
-                cnt[nx][ny] += cnt[cur.x][cur.y];
-
-                if(!visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    pq.add(new Node(nx,ny, board[nx][ny]));
-                }
+                cnt += dp[nx][ny];
             }
         }
 
-        System.out.println(cnt[x-1][y-1]);
+        return cnt;
     }
 }
